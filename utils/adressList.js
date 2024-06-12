9282e13a35669f7f93f845c4e3014cf4eac01b38
Victor Montoya <vmontoya.rugertek@iurem.com>
2024-06-12 00:27:28 -0400

    cominezo de hardhat para ejecutar en ether

---
+++ utils/adressList.js
@@ -0,0 +1,20 @@
+/* // ambas address son polygon zkEVM osea de la red e polygon de pancakeSwap
+const addressFactory = "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E"
+const addressRouter = "0x8cFe327CEc66d1C090Dd72bd0FF11d690C33a2Eb"
+// las address de los tokens 
+const addressFrom = "0xA8CE8aee21bC2A48a5EF670afCc9274C7bbbC035" //USDC polygon red
+const addressTo = "0xa2036f0538221a77A3937F1379699f44945018d0" // MATIC polygon red */
+
+ // Todo pero desde la Etherscan 
+const addressFactory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
+const addressRouter = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"
+// las address de los tokens 
+const addressFrom = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" // WETH
+const addressTo = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2" // SUSHI
+
+module.exports = {
+    addressFactory,
+    addressRouter,
+    addressFrom,
+    addressTo
+}
